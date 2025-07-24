55ce5c14bc0707bbd74e3eabbd31870d75133149
Jack <logan676@163.com>
2025-07-24 15:56:22 +0800

    feat: add compose preview activity

---
+++ cameralibrary/src/main/java/com/cgfay/camera/compose/CameraPreviewComposable.kt
@@ -0,0 +1,24 @@
+package com.cgfay.camera.compose
+
+import android.view.View
+import android.widget.FrameLayout
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.fragment.app.FragmentActivity
+import com.cgfay.camera.fragment.CameraPreviewFragment
+
+@Composable
+fun CameraPreviewScreen() {
+    val context = LocalContext.current
+    AndroidView(factory = {
+        FrameLayout(it).apply {
+            id = View.generateViewId()
+            if (context is FragmentActivity) {
+                context.supportFragmentManager.beginTransaction()
+                    .replace(id, CameraPreviewFragment(), "camera_compose")
+                    .commitNowAllowingStateLoss()
+            }
+        }
+    })
+}
