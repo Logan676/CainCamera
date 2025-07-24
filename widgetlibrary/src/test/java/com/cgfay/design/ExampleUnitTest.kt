6e69458556b4c470416bd3094296873208d5e434
Jack <logan676@163.com>
2025-07-24 20:10:27 +0800

    Convert widgetlibrary tests to Kotlin

---
+++ widgetlibrary/src/test/java/com/cgfay/design/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.cgfay.design
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test converted to Kotlin.
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
