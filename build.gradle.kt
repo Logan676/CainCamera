cad994dcdb9ec698f747e3a0ccb074759776a570
Jack <logan676@163.com>
2025-07-25 15:54:46 +0800

    Remove kotlinVersion property and update plugin

--- build.gradle.kt
+++ build.gradle.kt
@@ -9,7 +9,7 @@ buildscript {
     dependencies {
         // Updated to support compileSdk 35
         classpath 'com.android.tools.build:gradle:8.3.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:${project.property('kotlinVersion')}"
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0"
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
