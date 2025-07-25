94b1996d7972e9c61e13c8d6a6210d8494adca0e
Jack <logan676@163.com>
2025-07-25 13:51:55 +0800

    Convert facedetectlibrary build.gradle to Kotlin DSL

---
+++ facedetectlibrary/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = rootProject.extra["compileSdkVersion"] as Int
+    buildToolsVersion = rootProject.extra["buildToolsVersion"] as String
+    namespace = "com.cgfay.facedetectlibrary"
+
+    defaultConfig {
+        minSdk = rootProject.extra["minSdkVersion"] as Int
+        targetSdk = rootProject.extra["targetSdkVersion"] as Int
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    repositories {
+        flatDir {
+            dirs("libs")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(project(":landmarklibrary"))
+    implementation(project(":utilslibrary"))
+    implementation("androidx.appcompat:appcompat:${rootProject.extra["appcompatVersion"]}")
+    testImplementation("junit:junit:${rootProject.extra["junitVersion"]}")
+    androidTestImplementation("androidx.test.ext:junit:${rootProject.extra["androidXJunitVersion"]}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${rootProject.extra["espressoVersion"]}")
+    implementation(mapOf("name" to "MGFaceppSDK-0.5.2", "ext" to "aar"))
+    implementation(mapOf("name" to "MGLicenseManagerSDK-0.3.1", "ext" to "aar"))
+}
