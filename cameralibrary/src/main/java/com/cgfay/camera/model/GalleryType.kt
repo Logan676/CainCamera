3484e73a6aeabb1538226d6c8dda055f9e888a2d
Jack <logan676@163.com>
2025-07-25 16:29:11 +0800

    refactor: convert camera models to Kotlin

---
+++ cameralibrary/src/main/java/com/cgfay/camera/model/GalleryType.kt
@@ -0,0 +1,10 @@
+package com.cgfay.camera.model
+
+/**
+ * Created by cain.huang on 2017/9/28.
+ */
+enum class GalleryType {
+    PICTURE,        // \u56fe\u7247
+    VIDEO_60S,      // \u62cd60\u79d2
+    VIDEO_15S,      // \u62cd15\u79d2
+}
