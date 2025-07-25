390aeeba117b76436f4e3bc30e1e0896468d5d53
Jack <logan676@163.com>
2025-07-25 17:47:48 +0800

    Convert GLImageSurfaceView to Kotlin

--- filterlibrary/build.gradle.kt
+++ filterlibrary/build.gradle.kt
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id("kotlin-android")
 }
 
 android {
@@ -46,10 +47,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${rootProject.extra["kotlinVersion"]}")
     implementation(project(":landmarklibrary"))
     implementation(project(":utilslibrary"))
     implementation(project(":gdxlibrary"))
