dc7c738bdf408eca2829690548976fcc77896e95
Jack <logan676@163.com>
2025-07-26 13:37:52 +0800

    Refactor picker models with Compose

--- pickerlibrary/src/main/java/com/cgfay/picker/model/MimeType.kt
+++ pickerlibrary/src/main/java/com/cgfay/picker/model/MimeType.kt
@@ -1,21 +1,24 @@
 package com.cgfay.picker.model
 
 import androidx.collection.ArraySet
+import androidx.compose.runtime.Stable
 import java.util.Arrays
 import java.util.EnumSet
 import java.util.Set
 
 /**
- * 项目支持的媒体类型
+ * Supported media mime types.
  */
+@Stable
 enum class MimeType(val mimeType: String, val extensions: Set<String>) {
-    // 图片类型
+    // Images
     JPEG("image/jpeg", arraySetOf("jpeg")),
     JPG("image/jpg", arraySetOf("jpg")),
     BMP("image/bmp", arraySetOf("bmp")),
     PNG("image/png", arraySetOf("png")),
     GIF("image/gif", arraySetOf("gif")),
-    // 视频类型
+
+    // Videos
     MPEG("video/mpeg", arraySetOf("mpeg", "mpg")),
     MP4("video/mp4", arraySetOf("mp4", "m4v")),
     GPP("video/3gpp", arraySetOf("3gpp")),
@@ -29,11 +32,9 @@ enum class MimeType(val mimeType: String, val extensions: Set<String>) {
         @JvmStatic
         fun of(type: MimeType, vararg rest: MimeType): Set<MimeType> = EnumSet.of(type, *rest)
 
-        // 图片
         @JvmStatic
         fun ofImage(): Set<MimeType> = EnumSet.of(JPEG, JPG, PNG, BMP, GIF)
 
-        // 视频
         @JvmStatic
         fun ofVideo(): Set<MimeType> = EnumSet.of(MPEG, MP4, GPP, MKV, AVI)
 
