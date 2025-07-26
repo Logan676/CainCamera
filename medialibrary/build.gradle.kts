af46e22aae9920f702eb8392aa769415560e3f69
Jack <logan676@163.com>
2025-07-26 22:39:47 +0800

    Use kotlinVersion for compose plugin

--- medialibrary/build.gradle.kts
+++ medialibrary/build.gradle.kts
@@ -1,7 +1,9 @@
+val kotlinVersion: String by rootProject.extra
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion
 }
 
 val platformVersion = rootProject.extra["minSdkVersion"].toString()
